6922696aa26b8cb30b3e09b2cff31be9ad187d5a
aimanprasla <115961188+aimanprasla@users.noreply.github.com>
2022-10-16 22:04:36 -0500

    Add files via upload

---
+++ 4 Function Calculator/calculator.cpp
@@ -0,0 +1,23 @@
+//Simple 4 function calculator
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    char operation;
+    cout << "Enter the first number: " << endl;
+    cin >> num1;
+    cout << "Enter the second number: " << endl;
+    cin >> num2;
+    cout << "Enter the operation (+,-,*,/): " << endl;
+    cin >> operation;
+    if(operation == '+')
+        cout << "The sum of these two numbers is: " << num1 + num2;
+    else if(operation == '-')
+        cout << "The difference of these two numbers is: " << num1-num2;
+    else if(operation == '*')
+        cout << "The product of these two numbers is: " << num1*num2;
+    else
+        cout << "The quotient of these two numbers is: " << num1/num2;
+}
